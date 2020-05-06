7c89336f65ba3baa7715df93adc29fe6a7b3aa0f
Diego Laurentino <diego.r.laurentino@gmail.com>
2020-05-05 21:14:07 -0400

    Commented out jdbc connection string for Heroku.

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,13 +8,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:mysql://localhost:3306/conference_demo?useSSL=false&serverTimezone=UTC");
-        builder.username("root");
-        builder.password("root");
-        System.out.println("My custom datasource bean has been initialized and set.");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:mysql://localhost:3306/conference_demo?useSSL=false&serverTimezone=UTC");
+//        builder.username("root");
+//        builder.password("root");
+//        System.out.println("My custom datasource bean has been initialized and set.");
+//        return builder.build();
+//    }
 }
